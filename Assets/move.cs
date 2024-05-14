9e2611967d08937de3a9b16bdf61e8f047883200
Udhayasankaran04 <udhayasankaran04@gmail.com>
2024-05-14 21:16:37 +0530

    first commit

--- Assets/move.cs
+++ Assets/move.cs
@@ -2,25 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class move : MonoBehaviour
 {
-    
-   
+    public float speed=40.0f;
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void SpawnRandomAnimal()
-    {
-        
-    }
-
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+    }
 }
